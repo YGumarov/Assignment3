6e62b2b10be1cf1fbcc071196e04bdce04bd008d
YGumarov <yerassylgumarov@gmail.com>
2023-05-01 22:52:49 +0600

    tak tak

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -1,11 +1,10 @@
-﻿namespace Assignment3;
-
-public class MyHashTable<K, V> {
+﻿public class MyHashTable<K, V>
+{
     private class HashNode<K, V>
     {
-        private K key;
-        private V value;
-        private HashNode<K, V> next;
+        public K key;
+        public V value;
+        public HashNode<K, V> next;
 
         public HashNode(K key, V value)
         {
@@ -13,12 +12,12 @@ public class MyHashTable<K, V> {
             this.value = value;
         }
 
-        public String toString()
+        public override string ToString()
         {
-            return "{" + key + " " + value + "}";
+            return "{" + key.ToString() + " " + value.ToString() + "}";
         }
     }
-    
+
     private HashNode<K, V>[] chainArray;
     private int M = 11;
     private int size;
@@ -36,13 +35,50 @@ public class MyHashTable<K, V> {
         size = 0;
     }
 
-    private int hash(K key)
+    private int Hash(K key)
     {
-        int hashCode = key.GetHashCode();
-        return hashCode % M;
+        return (key.GetHashCode() & 0x7fffffff) % M;
     }
-    public void put(K key, V value) {…}
-    public V get(K key) {…}
+
+    public void Put(K key, V value)
+    {
+        int hash = Hash(key);
+        HashNode<K, V> node = chainArray[hash];
+        while (node != null)
+        {
+            if (node.key.Equals(key))
+            {
+                node.value = value;
+                return;
+            }
+
+            node = node.next;
+        }
+
+        HashNode<K, V> newNode = new HashNode<K, V>(key, value);
+        newNode.next = chainArray[hash];
+        chainArray[hash] = newNode;
+        size++;
+    }
+
+    public V Get(K key)
+    {
+        int hash = Hash(key);
+        HashNode<K, V> node = chainArray[hash];
+        while (node != null)
+        {
+            if (node.key.Equals(key))
+            {
+                return node.value;
+            }
+
+            node = node.next;
+        }
+
+        throw new ArgumentException("Key not found");
+    }
     public V remove(K key) {…}
     public bool contains(V value) {…}
     public K getKey(V value) {…}
+
+}
