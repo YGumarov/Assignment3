85c26e74f1395808dbf76fe978d52fc679cfd9f5
YGumarov <yerassylgumarov@gmail.com>
2023-05-01 23:11:57 +0600

    tak atk tak

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -77,8 +77,64 @@
 
         throw new ArgumentException("Key not found");
     }
-    public V remove(K key) {…}
-    public bool contains(V value) {…}
-    public K getKey(V value) {…}
 
+    public V remove(K key)
+    {
+        int hash = Hash(key);
+        HashNode<K, V> node = chainArray[hash];
+        HashNode<K, V> prev = null;
+        while (node != null)
+        {
+            if (node.key.Equals(key))
+            {
+                if (prev == null)
+                {
+                    chainArray[hash] = node.next;
+                }
+                else
+                {
+                    prev.next = node.next;
+                }
+                size--;
+                return node.value;
+            }
+            prev = node;
+            node = node.next;
+        }
+        throw new ArgumentException("Key not found");
+    }
+
+    public bool contains(V value)
+    {
+        for (int i = 0; i < chainArray.Length; i++)
+        {
+            HashNode<K, V> node = chainArray[i];
+            while (node != null)
+            {
+                if (node.value.Equals(value))
+                {
+                    return true;
+                }
+                node = node.next;
+            }
+        }
+        return false;
+    }
+
+    public K getKey(V value)
+    {
+        for (int i = 0; i < chainArray.Length; i++)
+        {
+            HashNode<K, V> node = chainArray[i];
+            while (node != null)
+            {
+                if (node.value.Equals(value))
+                {
+                    return node.key;
+                }
+                node = node.next;
+            }
+        }
+        return default(K);
+    }
 }
