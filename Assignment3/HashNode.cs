1c43baa71fb7e38c98dde41d682786faec761aff
YGumarov <yerassylgumarov@gmail.com>
2023-05-01 22:27:53 +0600

    Initial commit

---
+++ Assignment3/HashNode.cs
@@ -0,0 +1,17 @@
+﻿class HashNode<K, V>
+{
+    private K key;
+    private V value;
+    private HashNode <K, V> next;
+
+    public HashNode(K key, V value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String toString()
+    {
+        return "{" + key + " " + value + "}";
+    }
+}
